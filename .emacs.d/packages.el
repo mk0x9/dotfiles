83b5f7d1cc8448bee09c0c2d29207831a7b1d8f4
Michael Kuryshev <tensai@cirno.in>
2015-01-12 21:32:15 +0300

    add scss-mode

--- .emacs.d/packages.el
+++ .emacs.d/packages.el
@@ -2,7 +2,7 @@
   (require 'cl-lib))
 
 (defvar my-packages
-  '(window-numbering leuven-theme flycheck paredit smex twittering-mode haskell-mode json-mode company-ghc php-mode magit js2-mode js2-refactor)
+  '(window-numbering leuven-theme flycheck paredit smex twittering-mode haskell-mode json-mode company-ghc php-mode magit js2-mode js2-refactor scss-mode)
   "A list of packages to ensure are installed at launch.")
 
 (defun my-packages-installed-p ()
