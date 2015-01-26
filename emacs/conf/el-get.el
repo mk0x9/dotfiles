ea1c892dbca27c8d42497a80e37afa3d38312ec8
Michael Kuryshev <tensai@cirno.in>
2015-01-26 19:47:01 +0300

    scss-mode

--- emacs/conf/el-get.el
+++ emacs/conf/el-get.el
@@ -1,5 +1,5 @@
 (add-to-list 'load-path (mk9/concat-extensions-directory "el-get"))
 (require 'el-get)
 (setq el-get-byte-compile nil)
-(setq el-get-my-packages '(magit window-numbering smex js2-mode indent-guide json-mode twittering-mode mac-classic-theme json-mode auto-complete php-mode))
+(setq el-get-my-packages '(magit window-numbering smex js2-mode indent-guide json-mode twittering-mode mac-classic-theme json-mode auto-complete php-mode scss-mode))
 (el-get 'sync el-get-my-packages)
