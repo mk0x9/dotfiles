a5dd9506d9fa9d6e0fb12e34bcab4d8177708555
Michael Kuryshev <tensai@cirno.in>
2015-01-27 19:48:33 +0300

    delete cruft

--- emacs/conf/javascript-rirsrv.el
+++ emacs/conf/javascript-rirsrv.el
@@ -1,7 +1,3 @@
-(let ((fname "/home/cirno/Development/rirsrv/web.4talk.im/mkuryshev/src/api.js"))
-  (when (string-match-p "^.*/rirsrv/.*\\.js$" fname)
-    (setq js2-additional-externs '("setTimeout" "define" "btoa"))))
-
 (defun mk9/js2-rirsrv-hook ()
   (let ((fname buffer-file-name)
 	(regexp "^.*/rirsrv/.*\\.js$"))
