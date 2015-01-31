67ee69180470a45e372899e36c150fc2e68280ca
Michael Kuryshev <tensai@cirno.in>
2015-02-01 02:38:16 +0300

    disable auto-fill-mode in html-mode

--- emacs/conf/html.el
+++ emacs/conf/html.el
@@ -1,2 +1,7 @@
 (add-to-list 'auto-mode-alist '("\\.tpl" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.dust" . html-mode))
+
+(defun mk9/html-hook ()
+  (auto-fill-mode -1))
+
+(add-hook 'html-mode-hook 'mk9/html-hook)
