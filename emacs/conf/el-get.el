7f8ccccd973bc6916332ff54f7ed36f8602ebb5b
Michael Kuryshev <tensai@cirno.in>
2015-02-11 01:01:49 +0300

    go-mode

--- emacs/conf/el-get.el
+++ emacs/conf/el-get.el
@@ -4,5 +4,6 @@
 (setq el-get-my-packages '(magit window-numbering smex js2-mode indent-guide
 				 json-mode twittering-mode mac-classic-theme
 				 json-mode auto-complete php-mode scss-mode
-				 paredit vala-mode tern markdown-mode))
+				 paredit vala-mode tern markdown-mode go-mode
+				 go-autocomplete))
 (el-get 'sync el-get-my-packages)
