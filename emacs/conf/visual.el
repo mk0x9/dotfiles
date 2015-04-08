3b1a69f261a806e37c159d6d810cb33035eb17e5
Michael Kuryshev <me@mk9.name>
2015-04-08 18:54:12 +0300

    frames

--- emacs/conf/visual.el
+++ emacs/conf/visual.el
@@ -13,25 +13,27 @@
   (if (and (eq system-type 'gnu/linux)
 	   (eq window-system 'x)
 	   (not (boundp 'mk9/light-theme)))
-      (mk9/gtk-dark-variant frame)))
+      (mk9/gtk-dark-variant frame))
+  (mk9/set-font)
+  (mk9/set-theme))
 
-(add-hook 'after-make-frame-functions 'mk9/new-frame-hook)
-(mk9/new-frame-hook nil)
-
-(when window-system
+(defun mk9/set-font ()
   (set-default-font
    (if (boundp 'mk9/font)
        mk9/font
-     "Ubuntu Mono-13:weight=bold"))
+     "Ubuntu Mono-13:weight=bold") nil t)
   (dolist (range '((#x3000 . #x303f) (#x3040 . #x309f) (#x30a0 . #x30ff)
 		   (#xff00 . #xffef) (#x4e00 . #x9faf) (#x3400 . #x4dbf)))
     (set-fontset-font "fontset-default"
 		      range
 		      (if (boundp 'mk9/font-jp)
 			  mk9/font-jp
-			"Meiryo-12")))
-  (load-theme
-   (if (boundp 'mk9/theme) mk9/theme 'mac-classic)
-   t))
+			"Meiryo-12"))))
+
+(defun mk9/set-theme ()
+  (load-theme (if (boundp 'mk9/theme) mk9/theme 'mac-classic) t t))
+
+(add-hook 'after-make-frame-functions 'mk9/new-frame-hook)
+(mk9/new-frame-hook nil)
 
 (global-set-key [f10] 'menu-bar-mode)
