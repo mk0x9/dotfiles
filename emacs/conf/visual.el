ec879cc56e34afed9b1fe90fac3e1ead17aa0bb7
Michael Kuryshev <me@mk9.name>
2015-04-09 21:56:13 +0300

    emacsclient hooks

--- emacs/conf/visual.el
+++ emacs/conf/visual.el
@@ -3,19 +3,17 @@
 (defun mk9/gtk-dark-variant (frame)
   "Enable dark gtk+ theme for current frame"
   (call-process "xprop" nil t nil
-   		"-f" "_GTK_THEME_VARIANT" "8u"
-   		"-set" "_GTK_THEME_VARIANT" "dark"
-   		"-name" (if (framep frame)
-			    (frame-parameter frame 'name)
-			  (cdr (assoc 'name (frame-parameters))))))
+		"-f" "_GTK_THEME_VARIANT" "8u"
+		"-set" "_GTK_THEME_VARIANT" "dark"
+		"-name"
+		(frame-parameter (or frame (selected-frame)) 'name)))
 
 (defun mk9/new-frame-hook (frame)
   (if (and (eq system-type 'gnu/linux)
 	   (eq window-system 'x)
 	   (not (boundp 'mk9/light-theme)))
       (mk9/gtk-dark-variant frame))
-  (mk9/set-font)
-  (mk9/set-theme))
+  (mk9/set-font))
 
 (defun mk9/set-font ()
   (set-default-font
@@ -31,9 +29,18 @@
 			"Meiryo-12"))))
 
 (defun mk9/set-theme ()
-  (load-theme (if (boundp 'mk9/theme) mk9/theme 'mac-classic) t t))
+  (load-theme (if (boundp 'mk9/theme) mk9/theme 'mac-classic) t))
+(mk9/set-theme)
 
 (add-hook 'after-make-frame-functions 'mk9/new-frame-hook)
 (mk9/new-frame-hook nil)
+
+(defun mk9/emacsclient-hook ()
+  (let ((win-id (frame-parameter (selected-frame) 'outer-window-id)))
+    (call-process "wmctrl" nil nil nil "-i" "-r" win-id "-e" "0,100,100,-1,-1")
+    (call-process "wmctrl" nil nil nil "-i" "-R" win-id)
+    (mk9/gtk-dark-variant (selected-frame))))
+
+(add-hook 'server-switch-hook 'mk9/emacsclient-hook)
 
 (global-set-key [f10] 'menu-bar-mode)
