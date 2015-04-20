135e880bdba557879628b53e726765c259f9e94c
Michael Kuryshev <me@mk9.name>
2015-04-20 09:16:31 +0300

    .

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,6 +4,7 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.UrgencyHook
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Tabbed
+import XMonad.Util.EZConfig
 
 main = do c <- statusBar myBar myPP toggleStrutsKey myConfig
           xmonad c
@@ -16,13 +17,17 @@ myPP = xmobarPP { ppCurrent = xmobarColor "#72b932" ""
 
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
-myConfig = withUrgencyHook NoUrgencyHook defaultConfig { terminal    = "urxvt"
+myConfig = withUrgencyHook NoUrgencyHook defaultConfig { terminal    = "urxvt256c-mlc"
                                                        , modMask     = mod4Mask
                                                        , borderWidth = 0
                                                        , workspaces  = ["[ tty ]", "[emacs]", "[ web ]", "[ im ]", "[media]", "[misc]"] ++ map show [7..8] ++ ["[gimp]"]
                                                        , manageHook  = manageHook defaultConfig <+> manageDocks
                                                        , layoutHook  = tiled ||| tabbed shrinkText defaultTheme ||| Full
                                                        }
+           `additionalKeysP`
+           [ ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 5")
+           , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 5")
+           ]
            where tiled   = Tall nmaster delta ratio
                  nmaster = 1
                  ratio   = 1/2
