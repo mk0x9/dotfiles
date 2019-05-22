e2a7c86ff486a14b6c819116f5530939f6336d3b
Michael Kuryshev <me@mk9.name>
2019-05-22 19:02:21 +0200

    visuals

--- emacs/conf/local-bln23020716-pre.el
+++ emacs/conf/local-bln23020716-pre.el
@@ -1,5 +1,15 @@
 (setq mac-option-modifier 'meta)
 
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+ (set-face-attribute 'fringe nil
+                      :foreground (face-foreground 'default)
+                      :background (face-background 'default))
+
+(set-face-background 'vertical-border "gray")
+(set-face-foreground 'vertical-border (face-background 'vertical-border))
+(fringe-mode 12)
+
+
 ;(setq mk9/theme 'mac-classic)
 ;(setq mk9/theme 'sanityinc-tomorrow-day)
 (setq mk9/theme 'mac-classic)
