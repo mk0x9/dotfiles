b7f0d8672ee05e68c9bd529bf7ab406ac848c672
Michael Kuryshev <me@mk9.name>
2023-06-08 10:27:19 +0200

    add kubectl as a source to the tramp

--- emacs/conf/tramp.el
+++ emacs/conf/tramp.el
@@ -1,2 +1,11 @@
 (require 'tramp)
 (setq tramp-auto-save-directory "~/.emacs.d/tramp-autosave")
+
+(push
+ (cons
+  "kubectl"
+  '((tramp-login-program "kubectl")
+    (tramp-login-args (("exec" "-it") ("-u" "%u") ("%h") ("--") ("sh")))
+    (tramp-remote-shell "/bin/sh")
+    (tramp-remote-shell-args ("-i" "-c"))))
+ tramp-methods)
